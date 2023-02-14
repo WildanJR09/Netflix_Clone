6634e3d71163ebd870832f0b8aef5ea20617b50f
WildanJR09 <mwildjrs23@gmail.com>
2023-02-14 21:41:00 +0700

    deleting some stuff

--- firebase.ts
+++ firebase.ts
@@ -5,18 +5,6 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDOdIvHMQVeEPcicDuku62mhBU7CorAnoQ",
-//   authDomain: "netflix-clone-nextjs-91eb3.firebaseapp.com",
-//   projectId: "netflix-clone-nextjs-91eb3",
-//   storageBucket: "netflix-clone-nextjs-91eb3.appspot.com",
-//   messagingSenderId: "694172965831",
-//   appId: "1:694172965831:web:8a5b7b55928bbd9d3757e2",
-//   measurementId: "G-12FD59XFD9",
-// };
-
 const firebaseConfig = {
   apiKey: "AIzaSyD7DE0WqnPkmuN_cXBqfgp8-WDi7kfzfIk",
   authDomain: "netflix-clone-51a2a.firebaseapp.com",
